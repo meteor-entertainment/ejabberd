30ffbaa733a5bb93d32f4d2953c1fb45aa7aa2e0
BEmagical <brianpatrick.enriquez@gmail.com>
2013-05-20 23:22:30 -0700

    removed unnecessary markup, turned the Send Message textarea into an input with an inline Submit button so it sends on hitting the Enter key

--- apps/ejabberd/priv/assets/javascripts/application.js
+++ apps/ejabberd/priv/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('#login_pane').hide().delay(200).fadeIn('1000');
+	$('.main-content.auth').hide().delay(200).fadeIn('1000');
 	$('#change_subject').click(function() {
 		$('#form_setSubject').addClass('open');
 	});
